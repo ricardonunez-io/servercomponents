ee58e0653307b72ad233f30e6fce86a1be153860
Ricardo Nunez <113212961+ricardonunez0@users.noreply.github.com>
2023-06-07 15:18:27 -0700

    Update subtitle-transition.tsx

--- app/(posts)/subtitle-transition.tsx
+++ app/(posts)/subtitle-transition.tsx
@@ -1,32 +1,23 @@
-import { headers } from "next/headers";
+"use client";
+
+import { LazyMotion, domAnimation, m } from "framer-motion";
 
 export default function SubtitleTransition() {
-	const headersList = headers();
-	const referrer = headersList.get("referrer");
-
-	console.log(headersList);
-
-	/* if (referrer === "/") {
-		return (
-			<LazyMotion features={domAnimation}>
-				<motion.div
-					initial={{ height: "fitContent", opacity: 1 }}
-					animate={{ height: 0, opacity: 0 }}
-					transition={{ duration: 0.75, ease: "easeInOut" }}
-				>
-					<p className="text-2xl sm:text-3xl md:text-4xl font-bold">
-						... are complicated.
-					</p>
-					<p className="text-lg md:text-xl text-slate-500 dark:text-slate-400">
-						Here are some resources to make them easier to
-						understand and implement.
-					</p>
-				</motion.div>
-			</LazyMotion>
-		);
-	} else {
-		return <></>;
-	} */
-
-	return <></>;
+	return (
+		<LazyMotion features={domAnimation}>
+			<m.div
+				initial={{ height: "fitContent", opacity: 1 }}
+				animate={{ height: 0, opacity: 0 }}
+				transition={{ duration: 0.75, ease: "easeInOut" }}
+			>
+				<p className="text-2xl sm:text-3xl md:text-4xl font-bold">
+					... are complicated.
+				</p>
+				<p className="text-lg md:text-xl text-slate-500 dark:text-slate-400">
+					Here are some resources to make them easier to understand
+					and implement.
+				</p>
+			</m.div>
+		</LazyMotion>
+	);
 }
