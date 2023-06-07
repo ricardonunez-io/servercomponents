222cabf15de08859ae9fe4a11b769b9c5b0da996
Ricardo Nunez <113212961+ricardonunez0@users.noreply.github.com>
2023-06-07 14:48:14 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import ImportCost from "../public/import-cost.png";
 import NetworkWaterfall from "../public/network-waterfall.png";
 import Browser from "./browser";
 import Logo from "./logo";
+import { runtime } from "./(posts)/what-are-react-server-components/page";
+
+export const runtime = "edge";
+export const isStatic = true;
 
 const topics = [
 	{
