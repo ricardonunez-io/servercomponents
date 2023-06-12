564ca90e39ee4794dc4058ff9c91c87e4eac63f8
Ricardo Nunez <113212961+ricardonunez0@users.noreply.github.com>
2023-06-12 10:28:59 -0700

    Update pre.tsx

--- app/mdx-components/pre.tsx
+++ app/mdx-components/pre.tsx
@@ -7,17 +7,20 @@ export default function Pre({ children }: { children: ReactNode }) {
 	const preRef = useRef<HTMLPreElement | null>(null);
 
 	return (
-		<pre className="w-full my-4">
+		<pre className="w-full">
 			<code
 				ref={preRef}
-				className="group flex justify-between w-full p-6 m-1 font-mono rounded-xl bg-slate-800 overflow-x-scroll"
+				className="group flex justify-between w-full pt-6 pb-4 px-6 m-1 font-mono rounded-xl bg-slate-800 overflow-x-scroll"
 			>
 				{children}
-				<CopyToClipboard
-					getValue={() =>
-						preRef.current?.querySelector("code")?.textContent || ""
-					}
-				/>
+				<div className="-mt-2">
+					<CopyToClipboard
+						getValue={() =>
+							preRef.current?.querySelector("code")
+								?.textContent || ""
+						}
+					/>
+				</div>
 			</code>
 		</pre>
 	);
