a7ef69293f3622df3c1df29498545eec18745aa1
Ricardo Nunez <113212961+ricardonunez-io@users.noreply.github.com>
2023-06-21 11:04:37 -0700

    Added Github source component to header

--- app/(posts)/layout.tsx
+++ app/(posts)/layout.tsx
@@ -1,13 +1,13 @@
-import Browser from "../browser";
 import Logo from "../logo";
-import SubtitleTransition from "./subtitle-transition";
+import GithubLink from "@/components/github";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<main className="flex min-h-screen flex-col text-left">
 			<div className="gap-x-4 space-y-4">
-				<div className="sticky top-0 py-2 px-8 bg-gray-50 dark:bg-stone-950 border-b-2 border-gray-200 dark:border-gray-800 z-[100]">
+				<div className="sticky top-0 py-2 px-8 bg-gray-50 dark:bg-stone-950 border-b-2 border-gray-200 dark:border-gray-800 z-[100] flex justify-between items-center">
 					<Logo />
+					<GithubLink />
 				</div>
 				<div className="py-8 px-8 md:px-12 lg:px-16 lg:py-12 max-w-5xl mx-auto">
 					{children}
