1903e14446d93d84a59b99e77a99a96f7a0d03e0
Ricardo Nunez <113212961+ricardonunez-io@users.noreply.github.com>
2023-06-24 01:42:40 -0700

    Added animation and content to solutions page

--- app/(posts)/the-problems-rscs-solve/client-waterfall-diagram.tsx
+++ app/(posts)/the-problems-rscs-solve/client-waterfall-diagram.tsx
@@ -1,14 +1,15 @@
 export default function ClientWaterfallDiagram() {
 	return (
-		<div className="max-w-fit text-center space-y-4 py-8 text-gray-500 dark:text-gray-400 text-lg md:text-xl">
+		<div className="max-w-fit mx-auto text-center space-y-4 py-8 text-gray-500 dark:text-gray-400 text-lg md:text-xl">
 			<div className="hover:text-gray-700 dark:hover:text-gray-300">
 				Client makes <code>fetch()</code> request for profile
 			</div>
 			<div>↓</div>
 			<div className="hover:text-gray-700 dark:hover:text-gray-300">
-				Server gets that request, makes a query,{" "}
+				Server gets that request,{" "}
+				<br className="hidden sm:flex"/>makes a call to an external service (most likely),{" "}
 				<br className="hidden sm:flex" />
-				and sends a response back to the client
+				& sends the response back to the client
 			</div>
 			<div>↓</div>
 			<div className="hover:text-gray-700 dark:hover:text-gray-300">
