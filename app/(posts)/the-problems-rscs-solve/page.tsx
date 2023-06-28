a6c4798cd82a453cf9e55bcd0a1764e898c61b25
Ricardo Nunez <113212961+ricardonunez-io@users.noreply.github.com>
2023-06-27 23:46:44 -0700

    Adjusted metadata for both pages

--- app/(posts)/the-problems-rscs-solve/page.tsx
+++ app/(posts)/the-problems-rscs-solve/page.tsx
@@ -1,6 +1,11 @@
 import Solutions from "./solutions.mdx";
 
 export const runtime = "edge";
+export const metadata = {
+	title: "The Problems That React Server Components Solve",
+	description: "How React Server Components address data fetching, composing business logic, and bundle sizes",
+	publisher: "Ricardo Nunez",
+}
 
 export default function RSCsPage() {
 	return <Solutions />;
--- app/(posts)/what-are-react-server-components/page.tsx
+++ app/(posts)/what-are-react-server-components/page.tsx
@@ -3,9 +3,8 @@ import RSCs from "./rscs.mdx";
 export const runtime = "edge";
 export const metadata = {
 	title: "What are React Server Components?",
-	description: "A brief introduction to React Server Components and how they differ to Server-Side Rendering.",
+	description: "A brief introduction to React Server Components and how they differ to Server-Side Rendering",
 	publisher: "Ricardo Nunez",
-	
 }
 
 export default function RSCsPage() {
