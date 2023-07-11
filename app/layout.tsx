3d4c24a2eaf7e42b4694977041135dfe4bb38707
Ricardo Nunez <113212961+ricardonunez-io@users.noreply.github.com>
2023-07-11 14:11:32 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en">
 		{/* eslint-disable-next-line @next/next/no-head-element */}
+		<head>
+			<meta name="twitter:text:title" content="ServerComponents.dev | What are RSCs"/>
+		</head>
 		<body className="dark:bg-stone-950 dark:text-gray-300 dark:bg-dark-hero-pattern bg-gray-50 text-gray-700 bg-hero-pattern text-base font-medium md:text-lg overflow-x-hidden">
 		<div className="relative">
 			{children}
