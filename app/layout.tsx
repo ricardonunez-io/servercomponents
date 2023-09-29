027e92a4f80a9be7253e1e90da1557b1f9186e3a
Ricardo Nunez <113212961+ricardonunez-io@users.noreply.github.com>
2023-09-29 12:39:33 -0700

    changed bg color to be less warm

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,16 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en">
-		{/* eslint-disable-next-line @next/next/no-head-element */}
-		<head>
-			<meta name="twitter:text:title" content="ServerComponents.dev | What are RSCs"/>
-		</head>
-		<body className="dark:bg-stone-900 dark:text-gray-300 bg-gray-50 text-gray-700 text-base font-medium md:text-lg overflow-x-hidden">
-		<div className="relative">
-			{children}
-		</div>
-		<Footer/>
-		</body>
+			{/* eslint-disable-next-line @next/next/no-head-element */}
+			<head>
+				<meta name="twitter:text:title" content="ServerComponents.dev | What are RSCs" />
+			</head>
+			<body className="dark:bg-neutral-900 dark:text-gray-300 bg-gray-50 text-gray-700 text-base font-medium md:text-lg overflow-x-hidden">
+				<div className="relative">
+					{children}
+				</div>
+				<Footer />
+			</body>
 		</html>
 	);
 }
